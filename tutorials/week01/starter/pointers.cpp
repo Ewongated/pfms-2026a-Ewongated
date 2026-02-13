1c7049624a4c9334138081106c022f4f1ba1509b
Alen Alempijevic <Alen.Alempijevic@uts.edu.au>
2026-02-13 17:23:02 +1100

    Tutorial 1 Material

---
+++ tutorials/week01/starter/pointers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// Pointers are essential to C++ and are used to store the memory address of another variable.
+// They are declared by using the asterisk (*) before the variable name.
+// The data type of the pointer must be the same as the data type of the variable you're storing the address of.
+// When passed as a parameter to a function, the function can modify the value of the variable by using the pointer.
+
+int main () {
+
+    // Ex01
+//    * Assign to variable x (of type double) the value 41012
+    double x = 41012;
+    std::cout << "x = " << x << std::endl;
+
+//    * Use a pointer ip to point to x (why type is ip?)
+
+
+//    * Print the value of what ip is pointing to
+//    * Print the value of ip
+
+//    * Make variable y a reference to x (what type is y?)
+
+//    * Print the value of what y is referencing to
+
+//    * Create a double z of value 1
+
+//    * Use pointer ip to point to z
+
+//    * Assign z the value 100
+
+//    * Print the value of what ip is pointing to
+//    * Print the value of x
+//    * Print the value of y
+//    * Print the value of z
+
+    return 0;
+}
