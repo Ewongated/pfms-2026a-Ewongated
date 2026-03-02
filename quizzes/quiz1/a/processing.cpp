169434a5d547207a604a423765c25a03ee422aba
Ethan <ethanqcw@gmail.com>
2026-03-02 04:37:02 +0000

    Quiz1a

--- quizzes/quiz1/a/processing.cpp
+++ quizzes/quiz1/a/processing.cpp
@@ -5,11 +5,35 @@
 // Implement function according to specification in header file
 std::vector<Charger> lowestBatteryLevel(std::vector<Charger> fleet) {
     std::vector<Charger> result;
+    if (fleet.empty()) return {};
+
+    //find lowest
+    unsigned int lowest = fleet[0].getBatteryLevel();
+    for (auto& c : fleet)
+        if (c.getBatteryLevel() < lowest)
+            lowest = c.getBatteryLevel();
+    
+    //pushback all lowest values
+    for (auto& c : fleet)
+        if (c.getBatteryLevel() == lowest)
+            result.push_back(c);
+
     return result;
 }
 
 // Implement function according to specification in header file
 std::vector<Charger> eligibleForRecharge(std::vector<Charger> fleet, unsigned int batteryCutoff) {
     std::vector<Charger> result;
+    if (fleet.empty()) return {};
+
+    for (auto& c : fleet)
+        if (c.getBatteryLevel() < batteryCutoff && !c.getChargingStatus())
+            result.push_back(c);
+
+    // Sort Start,End, 
+    std::sort(result.begin(), result.end(), [](Charger& a, Charger& b) {
+        return a.getBatteryLevel() < b.getBatteryLevel();
+    });
+
     return result;
 }
