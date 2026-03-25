43e4e93f525ca4204550de4acc3a44f14fb2eccf
Alen Alempijevic <Alen.Alempijevic@uts.edu.au>
2026-03-25 21:17:48 +1100

    Adding quiz3 material

---
+++ quizzes/quiz3/b/analysis.cpp
@@ -0,0 +1,17 @@
+#include "analysis.h"
+
+#include <iostream> // Only here for showing the code is working
+#include <thread>
+
+Analysis::Analysis(std::shared_ptr<Radar> radarPtr):
+    radarPtr_(radarPtr)
+{
+
+}
+
+//! @todo
+//! TASK 1 - Refer to README.md and the Header file for full description
+void Analysis::computeScanningSpeed(unsigned int samples, double& scanningSpeed){
+
+  return;
+}
